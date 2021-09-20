86bef07cd30cd60383454517b37c08cc34bef0e5
104155 <59625525+104155@users.noreply.github.com>
2021-09-20 18:01:53 +0200

    refactor map.js, reset start btn with .innerText instead of .innerHTML

--- script/map.js
+++ script/map.js
@@ -200,7 +200,7 @@ function resetTracking() {
   // lineString.clear();
   // iconSource.clear(); //delets white point permanently
   // unSet('Line');
-  document.querySelector('.startStopBtn').innerHTML = 'START';
+  document.querySelector('.startStopBtn').innerText = 'START';
 }
 
 //Toggle start/ stop tracking, time interval pos update
